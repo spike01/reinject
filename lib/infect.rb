e3f320cc94b3b8b873966b5fa0fe6b8f24fb42c5
Spike Nox <spike01@gmail.com>
2014-09-07 00:09:19 +0100

    Recursive no longer shifts an extra number into the array

--- lib/infect.rb
+++ lib/infect.rb
@@ -1,25 +1,29 @@
 class Array
 
-  def infect(default = nil, &block)
+  def infect(default = nil, symb = nil, &block)
     memo = default || self.first
     start = (memo == default ? 0 : 1)
     self.drop(start).each { |element| memo = yield(memo, element) }  
     memo
   end
 
-  def reinject(memo = self.shift, &block)
+  def reinject(default = nil, symb = nil, &block)
     array = self.dup 
-    self.unshift(memo)
+    memo = default.nil? ? array.shift : default
     memo = yield(memo, array.shift)
     return memo if array.empty?
     array.reinject(memo, &block)
   end
+
+
+
+
 end
 
 # Given an array [1, 2, 3, 4, 5] and a &block { |x, y| x something y}
 # And an optional default value
 # When we yield the block
 # And store it back to memo
-# When we should have a shifted array [2, 3, 4, 5] ready to feed back into the
+# Then we should have a shifted array [2, 3, 4, 5] ready to feed back into the
 # method
 
--- spec/infect_spec.rb
+++ spec/infect_spec.rb
@@ -31,15 +31,15 @@ describe Array do
       expect(string_array.infect(&:+)).to eq(string_array.inject(&:+))
     end
 
-    #it 'can take a symbol as an argument' do
-      #expect(array.infect(:+)).to eq(array.inject(:+))
-    #end
+    it 'can take a symbol as an argument' do
+      expect(array.infect(:+)).to eq(array.inject(:+))
+    end
   end
 
  context 'it behaves like #inject, but using recursion' do
 
-    it 'doesn\'t mess with the starting array' do
-      array.reinject { |x, y| x + y }
+   it 'doesn\'t mess with the starting array when passed an argument' do
+      array.reinject(10) {|x, y| x + y }
       expect(array).to eq((1..10).to_a)
     end
 
@@ -55,8 +55,12 @@ describe Array do
       expect(array.reinject { |x, y| x - y }).to eq(array.inject {|x, y| x - y } )
     end 
 
+    #it 'can be called with an argument' do
+      #expect(array.reinject(20) { |x, y| x + y }).to eq(array.inject(20) {|x, y| x + y } )
+    #end
+
     it 'can be called with an argument' do
-      expect(array.reinject(20) { |x, y| x + y }).to eq(array.inject(20) {|x, y| x + y } )
+      expect(array.reinject(20) { |x, y| x + y }).to eq(75)
     end
 
     it 'can take a symbol block as an argument' do
